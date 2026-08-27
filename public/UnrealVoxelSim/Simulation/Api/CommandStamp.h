a19390cf8473ff68392684bcc6acefe2b350b77e
Dmitriy Monakhov <dimamon6032403@gmail.com>
2026-08-27 16:02:46 +0200

    TODOs added

--- public/UnrealVoxelSim/Simulation/Api/CommandStamp.h
+++ public/UnrealVoxelSim/Simulation/Api/CommandStamp.h
@@ -8,15 +8,14 @@
 
 namespace UnrealVoxelSim::Simulation::Api
 {
+	// TODO Potential cross-domain semantics leak. Simulation API has nothing to do with the concept of Commands. Usage pattern of this struct supports it: it is used by other modules, but not in Simulation domain itself.
+	struct CommandStamp final
+	{
+		TickIndex TargetTick;
+		CommandSourceId Source;
+		std::uint64_t SourceSequence{};
 
-struct CommandStamp final
-{
-    TickIndex TargetTick;
-    CommandSourceId Source;
-    std::uint64_t SourceSequence{};
-
-    [[nodiscard]] constexpr bool IsValid() const noexcept { return Source.IsValid() && SourceSequence != 0; }
-    auto operator<=>(const CommandStamp &) const = default;
-};
-
+		[[nodiscard]] constexpr bool IsValid() const noexcept { return Source.IsValid() && SourceSequence != 0; }
+		auto operator<=>(const CommandStamp&) const = default;
+	};
 } // namespace UnrealVoxelSim::Simulation::Api
